fa1bad99b7647f6d285501617f1c0c0ee1340cfc
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2022-10-28 12:15:14 -0400

    base commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import urllib, urllib.request
+from bs4 import BeautifulSoup
 
 url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
-data = urllib.request.urlopen(url)
-print(data.read().decode('utf-8'))
+data = urllib.request.urlopen(url).read().decode('utf-8')
+
+soup = BeautifulSoup(data, "xml")
+print(soup.prettify())
