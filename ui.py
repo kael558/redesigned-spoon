915172cff8665e15bc1bebd5e4eaa925ffa3634b
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2022-10-28 22:16:55 -0400

    clustering

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from main import *
+import copy
 
 st.title('Paper Searcher')
 
@@ -38,6 +39,34 @@ all_embeddings = np.vstack([nn_embeddings, query_embed])
 
 # Cluster them using dendrograms & Plot them
 model = fitModel(nn_embeddings)
+
+
+# level 0 = show each doc as own cluster, level n = 1 cluster
+def get_clusters(cluster_dict, cluster_combine_order, level=count):
+    clusters = cluster_dict.copy()
+    n = len(clusters)
+    for i in range(level):
+        values = cluster_combine_order[0]
+        cluster_combine_order = np.delete(cluster_combine_order, 0, axis=0)
+        clusters[n] = clusters[values[0]] + clusters[values[1]]
+        clusters.pop(values[0])
+        clusters.pop(values[1])
+        n+=1
+    return clusters
+
+cluster_combine_order = copy.deepcopy(model.children_)
+
+clusters = dict()
+for cluster in model.labels_:
+    clusters[cluster] = [cluster]
+
+n = cluster_combine_order
+
+print(get_clusters(clusters, cluster_combine_order, 8))
+
+
+
+
 linkages = plotDendrogram(model)
 
 # Map the nearest embeddings to 2d
